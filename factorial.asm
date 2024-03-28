a5e685f9819c20ef5f4846b241af0093cae794b4
Peter <github@peter.dev.br>
2024-03-28 02:30:24 -0300

    Add simple sum

--- factorial.asm
+++ factorial.asm
@@ -1,24 +1,36 @@
+global _start, _sum
+
+section .data
+    buf db "Hello, World!", 0xa
+
 section .text
 
-global _start
 _start:
-    push 1
-    push 5
+    push 0x1
+    push 0x5
     call _sum
     add esp, 8
+    mov ecx, eax
 
 
-global _sum
+    ; exit
+    mov eax, 0x1
+    mov ebx, 0
+    int 0x80
+
 _sum:
+    ; stack prep
     push ebp
     mov ebp, esp
     push ebx
+
+    ; the actual subroutine 
     mov eax, [ebp + 8]
     mov ebx, [ebp + 12]
-    mov eax, ebx
-
+    add eax, ebx;
+    
     ; restore
     pop ebx
-    mov esp, ebp
     pop ebp
+    ;mov esp, ebp
     ret
